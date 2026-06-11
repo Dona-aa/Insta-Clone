e390fc223d3bc79a68f848454bd10965265681a9
Dona-aa <edonaperaj4@gmail.com>
2026-06-11 12:28:50 +0200

    Fix session user query

--- src/lib/server/auth.js
+++ src/lib/server/auth.js
@@ -1,6 +1,6 @@
 import pool from './db.js';
 import bcrypt from 'bcrypt';
-import { hash, randomUUID } from 'crypto';
+import { randomUUID } from 'crypto';
 
 export async function hashPassword(password) {
 	return await bcrypt.hash(password, 10);
@@ -12,20 +12,23 @@ export async function verifyPassword(password, hash) {
 
 export async function createSession(userId) {
 	const sessionId = randomUUID();
-	const expiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000); // 7 days
+	const expiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000); // 30 days
+
 	await pool.execute('INSERT INTO sessions (id, user_id, expires_at) VALUES (?, ?, ?)', [
 		sessionId,
 		userId,
 		expiresAt
 	]);
+
 	return sessionId;
 }
 
 export async function validateSession(sessionId) {
 	const [rows] = await pool.execute(
-		'SELECT u.id, u.username, u.role FROM sessions s  JOIN users u ON s.user_id = u.id  WHERE s.id = ? AND s.expires_at > NOW()',
+		'SELECT u.id, u.username FROM sessions s JOIN users u ON s.user_id = u.id WHERE s.id = ? AND s.expires_at > NOW()',
 		[sessionId]
 	);
+
 	return rows[0] ?? null;
 }
 
